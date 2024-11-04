21a5b12a86d0a3693aba5baab0dd388893ec4b41
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-11-04 17:05:33 +0530

    program to print rectangular star pattern

--- 1)patterns/pattern1/index.js
+++ 1)patterns/pattern1/index.js
@@ -15,3 +15,15 @@ const pattern = () => {
   }
 };
 pattern();
+
+//By observing the above rectangular pattern , the number of rows and column are sam . We can get the number dynamically to print rectangualr pattern with different no of rows or columns
+const pattern1 = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let string = "";
+    for (let j = 1; j <= n; j++) {
+      string = string + " " + "*";
+    }
+    console.log(string);
+  }
+};
+pattern1(15);
