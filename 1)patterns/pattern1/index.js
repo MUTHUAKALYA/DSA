ca51a2d0766360a19fd5af7931f19e827de8c581
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-11-04 16:37:43 +0530

    program to print patterns

---
+++ 1)patterns/pattern1/index.js
@@ -0,0 +1,13 @@
+//write a program to print the following pattern
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+const pattern = () => {
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+};
+pattern();
