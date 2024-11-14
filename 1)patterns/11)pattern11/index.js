7a876f5677f24278056f18143012c649c2b419d8
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-11-14 19:57:02 +0530

    program to print inverted triangle pattern

--- 1)patterns/11)pattern11/index.js
+++ 1)patterns/11)pattern11/index.js
@@ -8,3 +8,19 @@
 // ***
 // **
 // *
+//4 th rule applies in this pattern. Observe symmetry.
+//n=5
+function rotatedTriangle(n) {
+  for (let i = 1; i <= 2 * n - 1; i++) {
+    let str = "";
+    let stars = i;
+    if (i > n) {
+      stars = 2 * n - i;
+    }
+    for (let j = 1; j <= stars; j++) {
+      str += "*" + " ";
+    }
+    console.log(str);
+  }
+}
+rotatedTriangle(5);
