a1011608b7ade71750ad0a8d07f44bddcfb4c5da
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-11-15 20:56:55 +0530

    program to print the number crown pattern

--- 1)patterns/13)pattern13/index.js
+++ 1)patterns/13)pattern13/index.js
@@ -3,3 +3,19 @@
 // 12    21
 // 123  321
 // 12344321
+const numberCrown = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    for (let j = 1; j <= i; j++) {
+      str += j + " ";
+    }
+    for (let k = 1; k <= 2 * (n - i); k++) {
+      str += " ";
+    }
+    for (let j = n - 1; j <= 1; j++) {
+      str += j + " ";
+    }
+    console.log(str);
+  }
+};
+numberCrown(4);
