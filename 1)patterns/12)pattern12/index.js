a2f530817fba6b2f3f1a11521d13a8fea25e2eb5
MUTHUAKALYA <muthuakalyaa@gmail.com>
2024-11-15 20:27:48 +0530

    program to print binary pattern

--- 1)patterns/12)pattern12/index.js
+++ 1)patterns/12)pattern12/index.js
@@ -1,6 +1,19 @@
-//write a program to print the following pattern
+//write a program to print the following Binary number Triangle pattern
 // 1
 // 0 1
 // 1 0 1
 // 0 1 0 1
 // 1 0 1 0 1
+const binaryNumberTriangle = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 == 1) {
+      num = 1;
+    } else num = 0;
+    for (let j = 1; j <= i; j++) {
+      str = num + " ";
+      num = 1 - num;
+    }
+    console.log(str);
+  }
+};
+binaryNumberTriangle(5);
